c41f751eea56e6abe814250de7dc78cc3143434b
Benjamin Lirio <liriob@arch10.cis.gvsu.edu>
2020-12-11 17:38:20 -0500

    Lab 3

--- cs-241/lab-3/BenLirio.sh
+++ cs-241/lab-3/BenLirio.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+#Variables
 filename=input.txt
 secondfilename=input2.txt
 
+# Task 1
 grep gvsu $filename 
 head -n 1 $filename
 grep gvsu $filename | awk '{print $2}'
-echo "TASK #2"
+
+# Task 2
 cp $filename $secondfilename
 sed -i '/^$/d' $secondfilename
 sed -i '/Linux/d' $secondfilename
+
+# Task 3
 sed -i -e "s/cis/CIS/g" $secondfilename
 read -p "Please enter a string to be replaced: " originalWord
 lines=$(grep ${originalWord} $secondfilename | wc -l)
+
+# Make sure the user entered in an existing word
 if [[ lines -eq 0 ]]
 then
 	echo "Word does not exist"
